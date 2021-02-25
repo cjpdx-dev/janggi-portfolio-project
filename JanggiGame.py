533b17c6cd5661f55aa2dbf57bffc60af77810ee
cjpdx <cjpdx.dev@gmail.com>
2021-02-25 17:18:02 -0500

    worked on dev journal for class structure and logical flow. need to finish data structure design and future task scheduling by tonight

---
+++ JanggiGame.py
@@ -0,0 +1,87 @@
+#
+# Author:       Chris Jacobs
+# Date:         2/25/2021
+# Assignment:   CS 162 Portfolio Assignment - Janggi
+#
+# Description:  A CLI version of Janggi, aka Korean Chess. Please view README.md and janggi_notes.txt
+#               for further details.
+#
+
+class JanggiGame:
+
+    def __init__(self):
+        pass
+
+
+class Player:
+
+    def __init__(self):
+        pass
+
+
+class Board:
+
+    def __init__(self):
+        pass
+
+
+class Position:
+
+    def __init__(self):
+        pass
+
+
+class Piece:
+
+    def __init__(self):
+        pass
+
+
+class General(Piece):
+
+    def __init__(self):
+        super().__init__()
+
+
+class Guard(Piece):
+
+    def __init__(self):
+        super().__init__()
+
+
+class Chariot(Piece):
+
+    def __init__(self):
+        super().__init__()
+
+
+class Elephant(Piece):
+
+    def __init__(self):
+        super().__init__()
+
+
+class Horse(Piece):
+
+    def __init__(self):
+        super().__init__()
+
+
+class Cannon(Piece):
+
+    def __init__(self):
+        super().__init__()
+
+
+class Soldier(Piece):
+
+    def __init__(self):
+        super().__init__()
+
+
+
+
+
+
+
+
