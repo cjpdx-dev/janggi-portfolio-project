fa50dd60a0f2492044c3a334665562b7786937fe
cjpdx <cjpdx.dev@gmail.com>
2021-02-25 20:39:46 -0500

    added more dev notes, completed what I think is a finished game board prototype that uses old school string formatting and accepts a 2d list of string arguments. kind of clunky but it works and I'm not being graded on it so I shouldn't spend too much time coming up with something elegant. At this point I'll just need to make a method in the Board class that returns what the DisplayDriver needs to work i.e. the 2d list of rows that have format fields i.e all i.e 10 rows plus 2 score rows

--- JanggiGame.py
+++ JanggiGame.py
@@ -79,6 +79,71 @@ class Soldier(Piece):
         super().__init__()
 
 
+class DisplayDriver:
+
+    def __init__(self):
+
+        self.line1 = "                                                            "
+        self.line2 = "             aa  bb  cc  dd  ee  ff  gg  hh  ii   BLUE: %s  "
+        self.line3 = "          ========================================          "
+        self.line4 = "       10 |--%s--%s--%s-|%s\-%s-/%s|-%s--%s--%s--| 10       "
+        self.line5 = "          |             |   \  /   |             |          "
+        self.line6 = "        9 |--%s--%s--%s-|%s--%s--%s|-%s--%s--%s--| 9        "
+        self.line7 = "          |             |   /  \   |             |          "
+        self.line8 = "        8 |--%s--%s--%s-|%s/-%s-\%s|-%s--%s--%s--| 8        "
+        self.line9 = "          |             |----------|             |          "
+        self.line10 = "        7 |--%s--%s--%s--%s--%s--%s--%s--%s--%s--| 7        "
+        self.line11 = "          |                                      |          "
+        self.line12 = "        6 |--%s--%s--%s--%s--%s--%s--%s--%s--%s--| 6        "
+        self.line13 = "          |                                      |          "
+        self.line14 = "        5 |--%s--%s--%s--%s--%s--%s--%s--%s--%s--| 5        "
+        self.line15 = "          |                                      |          "
+        self.line16 = "        4 |--%s--%s--%s--%s--%s--%s--%s--%s--%s--| 4        "
+        self.line17 = "          |             |----------|             |          "
+        self.line18 = "        3 |--%s--%s--%s-|%s\-%s-/%s|-%s--%s--%s--| 3        "
+        self.line19 = "          |             |   \  /   |             |          "
+        self.line20 = "        2 |--%s--%s--%s-|%s--%s--%s|-%s--%s--%s--| 2        "
+        self.line21 = "          |             |   /  \   |             |          "
+        self.line22 = "        1 |--%s--%s--%s-|%s/-%s-\%s|-%s--%s--%s--| 1        "
+        self.line23 = "          ========================================          "
+        self.line24 = "             aa  bb  cc  dd  ee  ff  gg  hh  ii   RED: %s   "
+        self.line25 = "                                                            "
+
+        self.board_rows = [self.line1, self.line2, self.line3, self.line4, self.line5, self.line6,
+                           self.line7, self.line8, self.line9, self.line10, self.line11, self.line12,
+                           self.line13, self.line14, self.line15, self.line16, self.line17, self.line18,
+                           self.line19, self.line20, self.line21, self.line22, self.line23, self.line24,
+                           self.line25]
+
+    def display_board(self, pieces):
+
+        i = 0
+        for row in self.board_rows:
+            if "%" in row:
+                print(row % tuple(pieces[i]))
+                i += 1
+            else:
+                print(row)
+
+
+display = DisplayDriver()
+all_placements = ["100"], \
+                 ["CH", "EL", "HR", "GU", "**", "GU", "EL", "HR", "CH"],\
+                 ["**", "**", "**", "**", "GE", "**", "**", "**", "**"],\
+                 ["**", "CA", "**", "**", "**", "**", "**", "CA", "**"],\
+                 ["SO", "**", "SO", "**", "SO", "**", "SO", "**", "SO"],\
+                 ["**", "**", "**", "**", "**", "**", "**", "**", "**"],\
+                 ["**", "**", "**", "**", "**", "**", "**", "**", "**"],\
+                 ["SO", "**", "SO", "**", "SO", "**", "SO", "**", "SO"],\
+                 ["**", "CA", "**", "**", "**", "**", "**", "CA", "**"],\
+                 ["**", "**", "**", "**", "GE", "**", "**", "**", "**"],\
+                 ["CH", "EL", "HR", "GU", "**", "GU", "EL", "HR", "CH"],\
+                 ["100"]
+
+display.display_board(all_placements)
+
+
+
 
 
 
